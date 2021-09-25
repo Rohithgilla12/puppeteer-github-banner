8ffbfb0bdef551bd53e1cf47c08472fb1b8569a4
Rohithgilla12 <gillarohith1@gmail.com>
2021-09-26 01:38:30 +0530

    Increase screenshot clarity :camera:

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,13 @@ const GITHUB_USERNAME = "Rohithgilla12";
 const GITHUB_CONTRIBUTION_SELECTOR =
   "#js-pjax-container > div.container-xl.px-3.px-md-4.px-lg-5 > div > div.flex-shrink-0.col-12.col-md-9.mb-4.mb-md-0 > div:nth-child(2) > div > div.mt-4.position-relative > div > div.col-12.col-lg-10 > div.js-yearly-contributions > div:nth-child(1)";
 
+const REMOVE_SELECTOR =
+  "#js-pjax-container > div.container-xl.px-3.px-md-4.px-lg-5 > div > div.flex-shrink-0.col-12.col-md-9.mb-4.mb-md-0 > div:nth-child(2) > div > div.mt-4.position-relative > div > div.col-12.col-lg-10 > div.js-yearly-contributions > div:nth-child(1) > div > div > div > div.float-left";
+
 const main = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  await page.setViewport({ width: 800, height: 800, deviceScaleFactor: 4 });
 
   await page.goto(`https://github.com/${GITHUB_USERNAME}`);
 
@@ -19,6 +23,12 @@ const main = async () => {
     },
   ]);
   await page.waitForSelector(GITHUB_CONTRIBUTION_SELECTOR);
+
+  // puppeteer hide the selected element
+  await page.evaluate((selector) => {
+    const element = document.querySelector(selector);
+    element.style.display = "none";
+  }, REMOVE_SELECTOR);
   const element = await page.$(GITHUB_CONTRIBUTION_SELECTOR);
   if (element) {
     await element.screenshot({ path: "contributions.png" });

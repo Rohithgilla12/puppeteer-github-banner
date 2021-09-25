7b11701f83c4aa6ebcb3cf61652f58d734f651c5
Rohithgilla12 <gillarohith1@gmail.com>
2021-09-26 01:31:22 +0530

    Dark Mode support

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,28 @@
-const puppeteer = require("puppeteer");
+import puppeteer from "puppeteer";
 
 const GITHUB_USERNAME = "Rohithgilla12";
+
+const GITHUB_CONTRIBUTION_SELECTOR =
+  "#js-pjax-container > div.container-xl.px-3.px-md-4.px-lg-5 > div > div.flex-shrink-0.col-12.col-md-9.mb-4.mb-md-0 > div:nth-child(2) > div > div.mt-4.position-relative > div > div.col-12.col-lg-10 > div.js-yearly-contributions > div:nth-child(1)";
 
 const main = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+
   await page.goto(`https://github.com/${GITHUB_USERNAME}`);
 
-  await page.waitForSelector(
-    "#js-pjax-container > div.container-xl.px-3.px-md-4.px-lg-5 > div > div.flex-shrink-0.col-12.col-md-9.mb-4.mb-md-0 > div:nth-child(2) > div > div.mt-4.position-relative > div > div.col-12.col-lg-10 > div.js-yearly-contributions > div:nth-child(1)"
-  );
-  const element = await page.$(
-    "#js-pjax-container > div.container-xl.px-3.px-md-4.px-lg-5 > div > div.flex-shrink-0.col-12.col-md-9.mb-4.mb-md-0 > div:nth-child(2) > div > div.mt-4.position-relative > div > div.col-12.col-lg-10 > div.js-yearly-contributions > div:nth-child(1)"
-  );
-
-  await element.screenshot({ path: "contributions.png" });
+  // Dark Mode
+  await page.emulateMediaFeatures([
+    {
+      name: "prefers-color-scheme",
+      value: "dark",
+    },
+  ]);
+  await page.waitForSelector(GITHUB_CONTRIBUTION_SELECTOR);
+  const element = await page.$(GITHUB_CONTRIBUTION_SELECTOR);
+  if (element) {
+    await element.screenshot({ path: "contributions.png" });
+  }
 
   await browser.close();
 

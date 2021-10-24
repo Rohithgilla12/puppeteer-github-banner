094352a2e1dddaf7ac1c75a2a9fd943f934d7328
rohithgilla <gillarohith1@gmail.com>
2021-10-24 11:17:16 +0530

    twitter check

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import puppeteer from "puppeteer";
 import { addTextToImage } from "./imageUtils";
+import fs from "fs";
+const TwitterV1 = require("twitter");
+
+const credentials = {
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+};
+
+const clientV1 = new TwitterV1(credentials);
 
 const GITHUB_USERNAME = "Rohithgilla12";
 
@@ -13,49 +24,78 @@ const CONTRIBUTION_SELECTOR =
   "#js-pjax-container > div.container-xl.px-3.px-md-4.px-lg-5 > div > div.flex-shrink-0.col-12.col-md-9.mb-4.mb-md-0 > div:nth-child(2) > div > div.mt-4.position-relative > div > div.col-12.col-lg-10 > div.js-yearly-contributions > div:nth-child(1) > h2";
 
 const main = async () => {
-  const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1000, height: 800, deviceScaleFactor: 1 });
+  try {
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1000, height: 800, deviceScaleFactor: 1 });
 
-  await page.goto(`https://github.com/${GITHUB_USERNAME}`, {
-    waitUntil: "networkidle2",
-  });
+    await page.goto(`https://github.com/${GITHUB_USERNAME}`, {
+      waitUntil: "networkidle2",
+    });
 
-  // Dark Mode
-  await page.emulateMediaFeatures([
-    {
-      name: "prefers-color-scheme",
-      value: "dark",
-    },
-  ]);
-  await page.waitForSelector(GITHUB_CONTRIBUTION_SELECTOR);
+    // Dark Mode
+    await page.emulateMediaFeatures([
+      {
+        name: "prefers-color-scheme",
+        value: "dark",
+      },
+    ]);
+    await page.waitForSelector(GITHUB_CONTRIBUTION_SELECTOR);
 
-  // puppeteer hide the selected element
-  await page.evaluate((selector) => {
-    const element = document.querySelector(selector);
-    element.style.display = "none";
-  }, REMOVE_SELECTOR);
+    // puppeteer hide the selected element
+    await page.evaluate((selector) => {
+      const element = document.querySelector(selector);
+      element.style.display = "none";
+    }, REMOVE_SELECTOR);
 
-  await page.evaluate((selector) => {
-    const element = document.querySelector(selector);
-    element.style.margin = "8px";
-    element.style.paddingTop = "16px";
-  }, CONTRIBUTION_SELECTOR);
+    await page.evaluate((selector) => {
+      const element = document.querySelector(selector);
+      element.style.margin = "8px";
+      element.style.paddingTop = "16px";
+    }, CONTRIBUTION_SELECTOR);
 
-  const element = await page.$(GITHUB_CONTRIBUTION_SELECTOR);
-  if (element) {
-    await element.screenshot({ path: "contributions.png" });
+    const element = await page.$(GITHUB_CONTRIBUTION_SELECTOR);
+    if (element) {
+      await element.screenshot({ path: "contributions.png" });
+    }
+
+    await browser.close();
+
+    console.log("Done creating the screenshot");
+
+    await addTextToImage("contributions.png");
+
+    console.log("Done editing the screenshot!");
+
+    const name = "final.png";
+
+    const base64 = fs.readFileSync(`${name}.png`, { encoding: "base64" });
+
+    clientV1.post(
+      "account/update_profile_banner",
+      {
+        banner: base64,
+      },
+      (err: any, data: any, response: { toJSON: () => any }) => {
+        console.log("err", err);
+        const json = response.toJSON();
+        console.log(json.statusCode, json.headers, json.body);
+
+        try {
+          console.log(data);
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    );
+  } catch (e) {
+    console.error(e);
   }
-
-  await browser.close();
-
-  console.log("Done creating the screenshot");
-
-  await addTextToImage("contributions.png");
-
-  console.log("Done editing the screenshot!");
 };
 
 main();
+setInterval(() => {
+  main();
+}, 12000);
